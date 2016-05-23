a4dd609ce82ba02047f9e8075058ddca15182a26
Svetlana Galkina <svegal@biosustain.dtu.dk>
2016-05-23 17:44:45 +0200

    Do not mount in spawner dockerfile

--- cobra-spawner/docker-entrypoint.sh
+++ cobra-spawner/docker-entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-# If the run command is the default, do some initialization first
-if [ "$(which "$1")" = "/usr/local/bin/start-singleuser.sh" ]; then
-  # Mount user's shared folder
-  : ${NOTEBOOK_DIR:=/home/$NB_USER/work}
-  if [ ! -d "$NOTEBOOK_DIR/$JPY_USER" ]; then
-    mkdir $NOTEBOOK_DIR/$JPY_USER
-  fi
-  mount -v -t cifs //dtu-storage.win.dtu.dk/$JPY_USER $NOTEBOOK_DIR/$JPY_USER -o username=$JPY_USER,password=$JPY_PASS,sec=ntlm
-fi
+# Pre-running commands
 
-# Run the command provided
 exec "$@"
